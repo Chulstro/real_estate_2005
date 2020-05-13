39faca82adb4afd8616622120a9cbb3219ba3a2e
Chandler Hulstrom <chulstro11@gmail.com>
2020-05-13 09:09:45 -0600

    Existing tests asserted

--- test/room_test.rb
+++ test/room_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/room'
+require 'pry'
 
 class RoomTest < Minitest::Test
   def test_it_exists
@@ -9,13 +10,11 @@ class RoomTest < Minitest::Test
   end
 
   def test_it_has_a_category
-    skip
     room = Room.new(:bedroom, 10, '13')
     assert_equal :bedroom, room.category
   end
 
   def test_it_can_get_area
-    skip
     room1 = Room.new(:bedroom, 10, '13')
     room2 = Room.new(:living_room, 15, '12')
     assert_equal 130, room1.area
